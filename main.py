17582f53066256d6b934bb92ad1bcadab365acb0
Rico Metzger <rico.metzger@gmail.com>
2026-06-01 13:53:22 +0300

    simple imports for console commands

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from invoicely!")
+import typer
+from rich import rprint
 
+from dotenv import load_dotenv
+
+app = typer.Typer()
+load_dotenv()
+
+@app.command()
+def hello(name: str):
+    rprint(f"[italic red]Hello[/italic red] {name}!", locals())
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Mr. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 if __name__ == "__main__":
-    main()
+    app()
